919ecc3d67557d539dff325f63506f1c7ad029f7
Gustavo Azevedo <gazevedo@brasilcap.com.br>
2020-12-22 18:06:17 -0300

    alterando o testSecret para mostrar como criptografar um conteúdo e inseri-lo em um arquivo para ser carregado no S3, como o properties encriptografado.

--- test/testSecret.js
+++ test/testSecret.js
@@ -1,22 +1,58 @@
-var BRCAPAWS = require('../index.js');
+const BRCAPAWS = require('../index.js');
 
-const properties = {
-    'id' : 1,
-    'valor' : 'teste'
-}
+const fs = require('fs');
 
-BRCAPAWS.Kms_encrypt('keyid', 'sa-east-1', JSON.stringify(properties), function(err, data){
-    
-    console.log('cifrando: ',JSON.stringify(properties));   
-    
-    if (err){
+//Preencher o JSON a ser criptografado e criado o arquivo para ser enviado para o S3"
+const properties = { "key": "<nameProperties>", "idTeste": "1", "urlTeste": "https://apis-dev.brasilcap.info/teste/darwin" };
+
+//Preencher com key Id do KMS na AWS, exemplo "properties-dev"
+const keyId = '<keyId_KMS';
+
+//Preencher o nome do arquivo a ser gerado com o JSON criptografado para ser enviado para o S3"
+const pathNameWriteFile = 'test/<arquivo-properties>'
+
+const regionKMS = 'sa-east-1'
+
+
+BRCAPAWS.Kms_encrypt(keyId, regionKMS, JSON.stringify(properties), function (err, data) {
+
+    console.log('cifrando: ', JSON.stringify(properties));
+
+    if (err) {
         console.log(err);
-    }else{        
-        BRCAPAWS.Kms_decrypt(data.CiphertextBlob, 'sa-east-1', function(err, dataDecripted){
-            if (err){
+    } else {
+        //console.log('DATA: ', data);
+        //console.log('data.CiphertextBlob: ', data.CiphertextBlob);
+        //console.log('Buffer DATA: ', Buffer.from(data.CiphertextBlob).toString("base64"));
+
+        fs.writeFileSync(pathNameWriteFile, Buffer.from(data.CiphertextBlob));
+
+        BRCAPAWS.Kms_decrypt(data.CiphertextBlob, regionKMS, function (err, dataDecripted) {
+            if (err) {
                 console.log(err);
-            }else{
+            } else {
                 console.log(dataDecripted);
+            }
+        });
+    }
+});
+
+//Inserir o nome do bucket do properties criptografado para buscar e descriptografar para verificar o conteudo
+const bucketS3 = '<bucketS3>';
+
+//Inserir o nome do arquivo do properties criptografado para buscar e descriptografar para verificar o conteudo
+const fileNameS3 = '<fileNameS3>';
+
+BRCAPAWS.S3_Get(bucketS3, fileNameS3, function (err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('data: ', data);
+        BRCAPAWS.Kms_decrypt(data.Body, 'sa-east-1', function (err, dataDecripted) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('S3_Get:', dataDecripted);
             }
         });
     }
